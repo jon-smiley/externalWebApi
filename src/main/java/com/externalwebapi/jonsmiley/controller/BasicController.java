c8170275d5b28078f9cd36b72ce912c9fcea4ec9
Jon Smiley <jasmiley0715@gmail.com>
2020-01-04 08:05:35 -0600

    EWA-1: Refactor basic setup.

--- src/main/java/com/externalwebapi/jonsmiley/controller/BasicController.java
+++ src/main/java/com/externalwebapi/jonsmiley/controller/BasicController.java
@@ -2,21 +2,20 @@ package com.externalwebapi.jonsmiley.controller;
 
 import com.externalwebapi.jonsmiley.controller.Authentication.JwtValidation;
 import com.externalwebapi.jonsmiley.model.BasicModel;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BasicController {
-    @RequestMapping("/stockapi")
+    //stock api
+    @RequestMapping(value = "/stockapi", method = RequestMethod.GET)
     public BasicModel GetStockPrices(
-            @RequestParam(value="value", defaultValue ="test1") String value,
+            @RequestParam(value="symbol", required = true) String symbol,
+            @RequestParam(value = "series", required = true) String series,
             @RequestHeader(name = "Authorization", required = true) String auth){
 
         boolean authenticate = JwtValidation.Validation(auth);
         if (authenticate) {
-            return new BasicModel(value);
+            return new BasicModel(symbol);
         } else {
             String fail = "fail";
             return new BasicModel(fail);
